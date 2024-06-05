371aeae34cec8d82a1d47529c7ef78561cdb39eb
Austiel <107131702+Austiel@users.noreply.github.com>
2024-06-05 22:37:15 +0800

    Update Disclaimer.dart
    eto bago na

--- finalinvitation/lib/Pages/Act1/Disclaimer.dart
+++ finalinvitation/lib/Pages/Act1/Disclaimer.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
 
-// akoako
+// eto bago na
 class Disclaimer extends StatefulWidget {
   const Disclaimer({Key? key}) : super(key: key);
 
