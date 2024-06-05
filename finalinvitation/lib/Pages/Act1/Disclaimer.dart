08edc66d342aaa5cb21101b0785b741a8b36cc3f
Austiel <107131702+Austiel@users.noreply.github.com>
2024-06-05 22:24:54 +0800

    asdddd
    asd

--- finalinvitation/lib/Pages/Act1/Disclaimer.dart
+++ finalinvitation/lib/Pages/Act1/Disclaimer.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
 
-// ASDASDASDASDASDASDASD
+// ako ay may lobo lumipad sa langit
 class Disclaimer extends StatefulWidget {
   const Disclaimer({Key? key}) : super(key: key);
 
